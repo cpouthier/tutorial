3a45da52ee94b9c49f72003d047ba57cf6ee5ebe
cpouthier <80251456+cpouthier@users.noreply.github.com>
2024-02-15 17:53:45 +0100

    commit

--- installscript.sh
+++ installscript.sh
@@ -1,24 +1,32 @@
-# Assisted deployment
-The aim of this tutorial is to guide you through a full deployment of Kasten on a single node K3s running on a Linux VM (tested with Ubuntu 22.04).
-K3s is a lightweight distribution of Kubernetes (K8s).
-## Pre-requisites
-The main pre-requisite is obviously to get a VM or bare metal server with Linux installed and superuser access. The superuser access (su) will be used in order to run scipts below and the fdisk utility to provide a new partition we will format in zfs.
-You also need to ensure to get **at least 8GB of memory and about 100GB of disk** to install all the tools, K3s.
-All instructions below will be run as superuser (sudo su).
-## Setup the environement
-First of all we need to tune a bit your Linux environement:
-```shell
+#! /bin/bash
+# This script will:
+#   Setup apt and tune the environment
+#   Setup username, password and drive path as environement variables for further reference
+#   Install Helm
+#   Install K3s without Traeffik
+#   Tune bash for kubectl command for autocompletion
+#   Install Minio and create one standard bucket and one immutable bucket
+#   Install zfs and configure a pool then configure the storage class in K3s
+#   Install NGINX
+#   Install Kasten K10 and expose dashboard
+#   Create one location profile for each Minio bucket
+#   Install Pacman app and expose it on port 80
+#   Set up a daily backup and export policy for Pacman
+# 
+# The following script will set the ubuntu service restart under apt to automatic
+clear
 sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 apt update
 sysctl fs.inotify.max_user_watches=524288
 sysctl fs.inotify.max_user_instances=512
 echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
 echo "fs.inotify.max_user_instances = 512" >> /etc/sysctl.conf
+# Installing apache2-utils to get htpasswd
 apt install apache2-utils -y
-```
-## Setup some environment variables
-Now we need to set up some environement variables that we will use later in this tutorial. Kasten will be installed with basic authentication, hence the need to provide a username and a password. Same credentials will be used too to connect to Minio:
-```shell
+clear
+# Setting up interactively some environment variables to run this script
+echo "Kasten will be installed with basic authentication, hence the need to provide a username and a password."
+echo "You will use also those credentials to connect to Minio."
 echo -e "\033[0;31m Enter the username: \e[0m"
 read username < /dev/tty
 echo -e "\033[0;31m Enter the password: \e[0m"
@@ -26,102 +34,68 @@ read password < /dev/tty
 htpasswd_entry=$(htpasswd -nbm "$username" "$password" | cut -d ":" -f 2)
 htpasswd="$username:$htpasswd_entry"
 echo "Successfully generated htpasswd entry: $htpasswd"
-```
-**WARNING: ensure you do not exit your shell otherwise you'll loose those variable and you won't be able to perform a clean install with all instructions below!**
-
-We also need to get the fresh new partition you created with fdisk utility to set up the zfs pool we will use to provide snapshot compatible storage to K3s.
-```shell
+sleep 3
 fdisk -l
 echo ""
-echo -e "\033[0;31m Identify and enter drive path of extra volume (ie /dev/sdb) to set up Kasten K10 zfs pool: \e[0m"
+echo -e "\033[0;31m Enter drive path of extra volume (ie /dev/sdb) to set up Kasten K10 zfs pool: \e[0m"
 read DRIVE < /dev/tty
-```
-# Install some tools
-## Install Helm
-First of all we will install Helm which is a packet manager for Kubernetes:
-```shell
+
+# Install Helm
+clear
+echo "Installing Helm..."
+sleep 2
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod +x ./get_helm.sh
 ./get_helm.sh
-```
-## Install Kubectl
-Then we need to install Kubectl which is the command line tool to communicate with Kubernetes and add autocompletion in bash. We'll also add an alias (k) to simplify further interaction in bash:
-```shell
+echo ""
+echo "Helm installed!"
+sleep 2
+
+#Install Kubectl for Linux AMD64
+clear
+echo "Installing kubectl"
+sleep 2
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+# Adding kubectl autocompletion to bash
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 source <(kubectl completion bash)
 echo "alias k=kubectl" | tee -a .bashrc /root/.bashrc
 alias k=kubectl
-```
-# Installing K3s
-K3s will be installed as a single node cluster. We will disable traefik (default ingress controller) as we'll use nginx.
-## Install K3s
-```shell
+echo ""
+echo "Kubectl installed!"
+sleep 2
+
+# Installing k3s single node cluster with local storage disabled 
+#OLD COMMAND curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable local-storage " sh -s -
+clear
+echo "Installing k3s"
+sleep 2
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable local-storage --disable=traefik" sh -s -
 mkdir /root/.kube
 cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
 chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
-```
-Wait 30 seconds in order for K3s to spin up and check the status:
-```shell
+# Checking k3s installation
+echo ""
+echo "Please wait 30s for k3s to spin up..."
+sleep 30
 k3s check-config
 kubectl cluster-info
 kubectl get nodes -o wide
-```
-## Configure storage 
-As already said, we will configure a ZFS storage pool which will provide snapshot functionnality for our kubernetes applications deployed on this K3s cluster.
+echo ""
+echo -e "\033[0;31m k3s installed, please review k3s information whitin the next 15sec... \e[0m"
+sleep 15
 
-### Install zfs and configure kasten-pool storage pool on associated drive
-```shell
-apt install zfsutils-linux open-iscsi jq -y
-zpool create kasten-pool $DRIVE
-```
-### Configure zfs storage class
-```shell
-kubectl apply -f https://openebs.github.io/charts/zfs-operator.yaml
 
-echo | kubectl apply -f - << EOF
-apiVersion: storage.k8s.io/v1
-kind: StorageClass
-metadata:
-  name: kasten-zfs
-parameters:
-  recordsize: "4k"
-  compression: "off"
-  dedup: "off"
-  fstype: "zfs"
-  poolname: "kasten-pool"
-provisioner: zfs.csi.openebs.io
-EOF
-```
-### Configure zfs Volume Snaspshot class
-```shell
-echo | kubectl apply -f - << EOF
-kind: VolumeSnapshotClass
-apiVersion: snapshot.storage.k8s.io/v1
-metadata:
-  name: kasten-zfs-snapclass
-  annotations:
-    snapshot.storage.kubernetes.io/is-default-class: “true”
-    k10.kasten.io/is-snapshot-class: "true"
-driver: zfs.csi.openebs.io
-deletionPolicy: Delete
-EOF
-```
-### Annotate the Storage Class
-```shell
-kubectl patch storageclass kasten-zfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-```
-# Installing Nginx
-We will now install Nginx as our ingress controller for our K3s cluster:
-```shell
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace nginx --create-namespace
-```
-# Installing Minio
-Now we will perform Minio installation outside of K3s. Minio will provide S3 object storage which will be used as external repository to export Kasten backups.
-```shell
+# Installing Minio for AMD64 outside K3s
+clear
+echo "Installing Minio"
+sleep 2
+echo ""
+echo "The script is about to install minio for linux AMD64, please ensure you're running on this platform type, otherwise exit this script!"
+echo ""
+sleep 10
 wget https://dl.min.io/server/minio/release/linux-amd64/minio -P /root
 chmod +x /root/minio
 mv /root/minio /usr/local/bin
@@ -139,52 +113,93 @@ curl https://dl.min.io/client/mc/release/linux-amd64/mc \
 chmod +x $HOME/minio-binaries/mc
 export PATH=$PATH:$HOME/minio-binaries/
 mc alias set my-minio http://127.0.0.1:9000 $username $password
-```
-## (Optional) Buckets creation
-With the commands below, you can optionnaly create some buckets we will use and configure in Kasten automatically, let's start by creating a my-minio alias :
-```shell
-mc alias set my-minio http://127.0.0.1:9000 $username $password
-```
-We will create now a standard S3 bucket:
-```shell
+
+#Create standard S3 bucket
 mc mb my-minio/s3-standard
-```
-And an immutable bucket:
-```shell
+#create immutable S3 bucket for compliance
 mc mb --with-lock my-minio/s3-immutable
 mc retention set --default COMPLIANCE "180d" my-minio/s3-immutable
-```
-# Kasten installation
-This is actually my favourite part!
-## Running pre-flight checks
-Running pre-flight checks (also referred as primer) is a way to enure that you'll be able install properly Kasten on your environment:
-```shell
-curl https://docs.kasten.io/tools/k10_primer.sh | bash
-```
-Pay attention to the output in order to fix any problem before proceeding to Kasten installation.
-## Add and update Helm repository
-```shell
+echo ""
+echo "Minio installed and configured with 2 buckets!"
+sleep 2
+
+# Install zfs and configure kasten-pool storage pool on associated drive
+clear
+echo "Installing zfs on $DRIVE"
+sleep 2
+apt install zfsutils-linux open-iscsi jq -y
+zpool create kasten-pool $DRIVE
+
+# Configure zfs storage class
+kubectl apply -f https://openebs.github.io/charts/zfs-operator.yaml
+
+echo | kubectl apply -f - << EOF
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: kasten-zfs
+parameters:
+  recordsize: "4k"
+  compression: "off"
+  dedup: "off"
+  fstype: "zfs"
+  poolname: "kasten-pool"
+provisioner: zfs.csi.openebs.io
+EOF
+
+echo | kubectl apply -f - << EOF
+kind: VolumeSnapshotClass
+apiVersion: snapshot.storage.k8s.io/v1
+metadata:
+  name: kasten-zfs-snapclass
+  annotations:
+    snapshot.storage.kubernetes.io/is-default-class: “true”
+    k10.kasten.io/is-snapshot-class: "true"
+driver: zfs.csi.openebs.io
+deletionPolicy: Delete
+EOF
+
+
+kubectl patch storageclass kasten-zfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+echo ""
+echo "ZFS installed!"
+sleep 2
+
+#Install NGINX
+clear 
+echo "Installing NGINX"
+sleep 2
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace nginx --create-namespace
+echo ""
+echo "NGINX installed!"
+sleep 2
+
+# Install Kasten K10
+clear
+echo "Installing Kasten"
+sleep 2
+# Adding and updating Helm repository
 helm repo add kasten https://charts.kasten.io
 helm repo update
-```
-## Create the namespace for Kasten
-```shell
+# Run Kasten k10 primer
+curl https://docs.kasten.io/tools/k10_primer.sh | bash
+echo "Please exit this script within the next 15sec to fix any error before installing Kasten K10."
+sleep 15
+# Create kasten-io namespace
 kubectl create ns kasten-io
-```
-## Install Kasten
-Kasten will be installed in the kasten-io namespace with basic authentication:
-```shell
+# Install Kasten in the kasten-io namespace with basic authentication
 helm install k10 kasten/k10 --namespace kasten-io --set "auth.basicAuth.enabled=true" --set auth.basicAuth.htpasswd=$htpasswd
-```
-Other Helm options are available here: https://docs.kasten.io/latest/install/advanced.html?highlight=advanced#complete-list-of-k10-helm-options
-
-We will now find the Kasten gateway pod name and expose it:
-```shell
+echo ""
+echo "Please wait for 30sec whilst we wait for the pods to spin up..."
+echo "After this period the external URL for K10 access will display (DO NOT exit this script)"
+sleep 30
+echo ""
+# Finding the Kasten K10 gateway namespace name
 pod=$(kubectl get po -n kasten-io |grep gateway | awk '{print $1}' )
+# Expose the gateway pod through the load balancer on port 8000
 kubectl expose po $pod -n kasten-io --type=LoadBalancer --port=8000 --name=k10-dashboard
-```
-And setup the Kasten ingress:
-```shell
+
+# Setting up Kasten k10 ingress
 echo | kubectl apply -f - << EOF
 apiVersion: networking.k8s.io/v1
 kind: Ingress
@@ -204,10 +219,8 @@ spec:
                 port:
                   number: 8000
 EOF
-```
-## (Optional) Pre-populate Kasten
-### Accept EULA
-```shell
+
+#Accept EULA
 echo | kubectl apply -f - << EOF
 apiVersion: v1
 data:
@@ -223,24 +236,20 @@ clear
 echo ""
 echo "Kasten is now installed"
 sleep 2
-```
-### Create location profiles in Kasten
-#### Create the Minio access key and secret
-##### Create Minio access key
-```shell
+
+###Create Profiles in Kasten
+
+#Create Minio access key
 minio_access_key_id=$(echo $username)
 minio_access_key_secret=$(echo $password)
-```
-##### Create Minio secret for K10
-```shell
+#Create Minio secret for K10
 kubectl create secret generic k10-s3-secret-minio \
       --namespace kasten-io \
       --type secrets.kanister.io/aws \
       --from-literal=aws_access_key_id=$minio_access_key_id\
       --from-literal=aws_secret_access_key=$minio_access_key_secret
-```
-#### Create Location profile for Minio standard bucket
-```shell
+
+#Create Location profile for Minio Standard bucket
 echo | kubectl apply -f - << EOF
 apiVersion: config.kio.kasten.io/v1alpha1
 kind: Profile
@@ -264,9 +273,8 @@ spec:
       objectStoreType: S3
       skipSSLVerify: true
 EOF
-```
-#### Create Location profile for Minio immutable bucket
-```shell
+
+#Create Location profile for Minio Immutable bucket
 echo | kubectl apply -f - << EOF
 apiVersion: config.kio.kasten.io/v1alpha1
 kind: Profile
@@ -290,9 +298,8 @@ spec:
       objectStoreType: S3
       skipSSLVerify: true
 EOF
-```
-### Enable Kasten daily Kasten reports
-```shell
+
+# Enable Kasten daily Kasten reports
 echo | kubectl apply -f - << EOF
 kind: Policy
 apiVersion: config.kio.kasten.io/v1alpha1
@@ -332,17 +339,18 @@ spec:
       reportParameters:
         statsIntervalDays: 1
 EOF
-```
-# (Optional) Install Pacman application
-You can now install an application to test Kasten backup/restore operations. We will use a simple pacman game application which contains also a MongoDB.
-```shell
+
+# Install Pacman application
+clear 
+echo "Installing Pacman"
+sleep 2
 helm repo add pacman https://shuguet.github.io/pacman/
 helm install pacman pacman/pacman -n pacman --create-namespace --set ingress.create=true --set ingress.class=nginx
-```
-This application will be exposed on the web on port 80 of your server.
-## Create a daily backup policy for pacman
-This policy will create a dayly backup of pacman in crash consistent mode. Kasten is also able to manage application consistent backup with the use of blueprints (pre-snapshot and post- snapshot hooks) but this is not detailed (useful) in this example.
-```shell
+echo ""
+echo "Pacman is now installed!"
+sleep 2
+
+# Create a Daily backup policy for pacman
 echo | kubectl apply -f - << EOF
 apiVersion: config.kio.kasten.io/v1alpha1
 kind: Policy
@@ -372,10 +380,8 @@ spec:
     retention: {}
   
 EOF
-```
-# Final stage
-We will now save all credentials and URLs in a file for further reference and clean up
-```shell
+
+# Save credentials and URLs for further reference
 cat <<EOF > credentials
 Kasten k10 can be accessed on http://$get_ip:8000/k10/#/ using credentials ($username/$password)
 Minio console is available on  http://$get_ip:9001, with the same username/password.
@@ -385,6 +391,7 @@ Minio console is available on  http://$get_ip:9001, with the same username/passw
     Both of them can be accessed through API on http://$get_ip:9000 using credentials ($username/$password)
 Pacman is available on http://$get_ip
 EOF
+# Finish
 rm get_helm.sh
 rm k10primer.yaml
 clear
@@ -404,9 +411,3 @@ echo ""
 echo "Have fun!"
 echo ""
 sleep 4
-```
-# One more thing...
-If you're already fed up with the idea to copy/paste instructions, just run the command below as superuser (sudo su):
-```shell
-sudo curl -s https://raw.githubusercontent.com/cpouthier/ubuntu-k3s-minio-K10/main/installscript.sh | bash
-```
