9879688398eabdf51b2ee8e5b12d7ab0fb4bfb51
Clarence Pouthier <80251456+cpouthier@users.noreply.github.com>
2025-12-29 15:40:22 +0100

    Add Helm commands to install and configure Pacman with MongoDB

--- installscript.sh
+++ installscript.sh
@@ -353,6 +353,18 @@ clear
 echo "Installing Pacman"
 sleep 2
 helm repo add pacman https://shuguet.github.io/pacman/
+helm repo update
+helm pull pacman/pacman --untar
+cd pacman
+#create a values-override.yaml file to customize your deployment with:
+cat > values-override.yaml <<'EOF'
+mongodb:
+  image:
+    registry: docker.io
+    repository: bitnamilegacy/mongodb
+EOF
+#check default values for mongodb in the bitnami chart:
+helm template pacman . -f values-override.yaml | grep -i "bitnami"
 helm install pacman pacman/pacman -n pacman --create-namespace --set ingress.create=true --set ingress.class=nginx
 echo ""
 echo -e "\033[0;32m Pacman is now installed, but you may need additional time to access it so it gets a valid network access with nginx (depending on you local machine resources)\e[0m"
